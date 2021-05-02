26069e7739524c7bcb63b76628b60b77c6fdf70f
Orcan Kolankaya <orcan@bt-studio>
2021-05-02 10:10:41 -0400

    company-lsp code region is removed

--- init.el
+++ init.el
@@ -42,14 +42,14 @@
  '(lsp-ui-doc-include-signature t t)
  '(lsp-ui-doc-max-height 30 t)
  '(lsp-ui-doc-max-width 120 t)
- '(lsp-ui-doc-position (quote top) t)
+ '(lsp-ui-doc-position 'top t)
  '(lsp-ui-doc-use-childframe nil t)
  '(lsp-ui-doc-use-webkit nil t)
  '(lsp-ui-flycheck-enable t t)
  '(lsp-ui-imenu-enable t t)
- '(lsp-ui-imenu-kind-position (quote top) t)
+ '(lsp-ui-imenu-kind-position 'top t)
  '(lsp-ui-peek-enable t t)
- '(lsp-ui-peek-fontify (quote on-demand) t)
+ '(lsp-ui-peek-fontify 'on-demand t)
  '(lsp-ui-peek-list-width 50 t)
  '(lsp-ui-peek-peek-height 20 t)
  '(lsp-ui-sideline-code-actions-prefix "" t)
@@ -60,11 +60,9 @@
  '(lsp-ui-sideline-show-hover t t)
  '(lsp-ui-sideline-show-symbol t t)
  '(package-selected-packages
-   (quote
-    (wgrep define-word engine-mode dired-subtree eterm-256color xterm-color auto-yasnippet yasnippet-classic-snippets yasnippet evil-smartparens smartparens beacon dumb-jump counsel-projectile cheatsheet evil-collection auctex pdf-tools doom-modeline all-the-icons spacemacs-theme doom-themes rainbow-delimiters ivy-xref dap-mode cmake-ide cmake-font-lock cmake-mode company-c-headers company-lsp ccls lsp-treemacs lsp-ui lsp-mode company treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs flycheck counsel gnu-elpa-keyring-update ace-window org-bullets which-key try nlinum-relative diminish use-package)))
+   '(wgrep define-word engine-mode dired-subtree eterm-256color xterm-color auto-yasnippet yasnippet-classic-snippets yasnippet evil-smartparens smartparens beacon dumb-jump counsel-projectile cheatsheet evil-collection auctex pdf-tools doom-modeline all-the-icons spacemacs-theme doom-themes rainbow-delimiters ivy-xref dap-mode cmake-ide cmake-font-lock cmake-mode company-c-headers company-lsp ccls lsp-treemacs lsp-ui lsp-mode company treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs flycheck counsel gnu-elpa-keyring-update ace-window org-bullets which-key try nlinum-relative diminish use-package))
  '(projectile-project-root-files-top-down-recurring
-   (quote
-    ("compile_commands.json" ".ccls" "compile_commands.json" ".ccls" ".svn" "CVS" "Makefile"))))
+   '("compile_commands.json" ".ccls" "compile_commands.json" ".ccls" ".svn" "CVS" "Makefile")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
